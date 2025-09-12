06ce154979e62caf02063f6c650dd373c9d6d3e3
meloqp <meloqswork@gmail.com>
2025-09-12 20:56:24 +0300

    output answer in label now and add some elements

--- creation_sums.py
+++ creation_sums.py
@@ -9,3 +9,7 @@ def output(low_border, high_border, num_of_slag):
         num_list.append(signs_list[random.randrange(len(signs_list))])
     num_list.append(random.randint(low_border, high_border))
     return "".join(list(map(str, num_list)))
+
+def check(correct_answer, user_answer):
+    return correct_answer == user_answer
+
--- mathapp.py
+++ mathapp.py
@@ -2,18 +2,25 @@ import creation_sums
 import tkinter
 from tkinter import ttk
 
+correct_answer = None
+
 def enter_values():
-    listbox_output.delete(0, tkinter.END)
+    label_output_sums.config(text="")
     lower_border = int(entry_low_border.get())
     higher_border = int(entry_high_border.get())
     num_of_slags = int(entry_number_of_slag.get())
-    s = creation_sums.output(lower_border, higher_border, num_of_slags)
-    listbox_output.insert(0, s)
+    label_output_sums.config(text= creation_sums.output(lower_border, higher_border, num_of_slags))
+    global correct_answer
+    correct_answer = creation_sums.output(lower_border, higher_border, num_of_slags)
+    label_output_sums.grid(row=3, column=0, columnspan=3)
     return lower_border, higher_border, num_of_slags
+
 
 root = tkinter.Tk()
 root.title("mathapp")
 root.geometry("590x480")
+
+label_output_sums = ttk.Label(text="Sums here", font=("Times New Roman", 20), wraplength=550)
 
 label_low_border = ttk.Label(text="Low border")
 label_low_border.grid(row=0, column=0)
@@ -33,10 +40,16 @@ entry_number_of_slag.grid(row=1, column=1)
 entry_high_border = ttk.Entry()
 entry_high_border.grid(row=1, column=2)
 
-button_enter_values = ttk.Button(text= "enter border values", command=enter_values)
-button_enter_values.grid(row=2, column=1)
+where_write_answers_label = ttk.Label(text="Write answer here")
+where_write_answers_label.grid(row=5, column=1)
 
-listbox_output = tkinter.Listbox()
-listbox_output.grid(row=3, column=0, columnspan=3, sticky="nsew", padx=10)
+entry_answer = ttk.Entry()
+entry_answer.grid(row=6, column=0, columnspan=3)
+
+button_check_answers = ttk.Button(text="Check answer", command=creation_sums.check(correct_answer, int(entry_answer.get())))
+button_check_answers.grid(row=7, column=0, columnspan=3)
+
+button_create_sums = ttk.Button(text= "Create sums", command=enter_values)
+button_create_sums.grid(row=2, column=1)
 
 root.mainloop()
